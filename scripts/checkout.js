3aae97ef2afc10ae7c299732a3f6919ec5171a46
bezorek <maciejszymanskki@gmail.com>
2024-11-06 16:39:47 +0100

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,13 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
+import { loadProducts } from "../data/products.js";
 //import '../data/car.js'
-import '../data/backend-practise.js'
+//import '../data/backend-practise.js'
 //import '../data/cart-class.js'
 
-renderOrderSummary();
-renderPaymentSummary();
-renderCheckoutHeader();
+loadProducts(() => {
+   renderOrderSummary();
+   renderPaymentSummary();
+   renderCheckoutHeader();
+});
