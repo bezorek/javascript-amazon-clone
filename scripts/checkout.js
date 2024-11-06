e0ac8b06b589fc3863bef853846cb93de1a57feb
bezorek <maciejszymanskki@gmail.com>
2024-11-06 15:56:02 +0100

    Add backend practise file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,8 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import '../data/car.js'
+//import '../data/car.js'
+import '../data/backend-practise.js'
 //import '../data/cart-class.js'
 
 renderOrderSummary();
